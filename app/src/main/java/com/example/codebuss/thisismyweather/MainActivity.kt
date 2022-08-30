91eec2cdbebc20d49f38aedb40051450b5244c7a
mTifiy <m.kalich1993@gmail.com>
2022-08-30 14:03:52 +0300

    Lesson 3.2 1.02

--- app/src/main/java/com/example/codebuss/thisismyweather/MainActivity.kt
+++ app/src/main/java/com/example/codebuss/thisismyweather/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.codebuss.thisismyweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        main_citi_name.text = "Кропивницкий"
+//        main_day_data.text = "30 августа"
     }
 }
