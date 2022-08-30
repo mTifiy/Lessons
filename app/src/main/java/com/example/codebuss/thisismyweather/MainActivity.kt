f7b40f26a229f1c787815d8a249edec7c3aa059a
mTifiy <m.kalich1993@gmail.com>
2022-08-30 14:36:50 +0300

    Lesson 3.2 end of lesson

--- app/src/main/java/com/example/codebuss/thisismyweather/MainActivity.kt
+++ app/src/main/java/com/example/codebuss/thisismyweather/MainActivity.kt
@@ -8,7 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        main_citi_name.text = "Кропивницкий"
-//        main_day_data.text = "30 августа"
+        main_citi_name.text = "Кропивницкий"
+        main_day_data.text = "30 августа"
+        main_weather_condition_icon.setImageResource(R.drawable.ic_sun)
+        main_temp.text = "25\u00B0"
+        main_temp_min.text = "19"
+        main_temp_max.text = "25"
+        main_image_of_weather.setImageResource(R.mipmap.cloud3x)
+
     }
 }
